18e1bcd1f26643e4610c5aad38c8817709d2ce46
Daniel Adamlu <adamludnl@gmail.com>
2024-12-02 21:59:46 +0700

    feat: API routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\AnalysisController;
+use App\Http\Controllers\CvController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'cv'], function () {
+    Route::get('/get-all', [CvController::class, 'getAll']);
+    Route::post('/get-by-analysis-id', [CvController::class, 'getByAnalysisId']);
+    Route::post('/create', [CvController::class, 'create']);
+});
+
+Route::group(['prefix' => 'analysis'], function () {
+    Route::get('/get-all', [AnalysisController::class, 'getAll']);
+    Route::post('/get-by-user-id', [AnalysisController::class, 'getByUserId']);
+    Route::post('/save-result', [AnalysisController::class, 'saveResult']);
 });
